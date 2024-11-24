aa128e533785cf1760a0526a95355937694d0abb
maryam yusuf-awari <maryamyusufawari@gmail.com>
2024-11-23 20:53:46 -0800

    (Fourth practice)

---
+++ C.A test/m.yusuf-awari.rs
@@ -0,0 +1,25 @@
+use std::io;
+
+fn main|() {
+	#[derive(Debug)]
+	struct Name {
+		field: hospital
+	}
+   let name= String::new();
+   let date_of_birth= in::new();
+   let email_address= String::new();
+   let phone_number= in::new();
+   let number_of_siblings= in::new();
+   let medical_diagnosis= String::new();
+   let village
+
+
+if age >= 50 
+if children >= 4 {
+   println!("(12000000-20*12000000)");
+}
+else {
+	println!("oops, the discount doesn't apply");
+}
+
+let arrythmia= (550000-550000*5 )
