358dcfcd92d0b6a12ae0eb12c2304e6066f7f869
maryam yusuf-awari <maryamyusufawari@gmail.com>
2024-11-27 15:45:31 -0800

    (practice 1)

---
+++ week-6/pratice_1/target/debug/deps/pratice_1.d
@@ -0,0 +1,5 @@
+C:\Users\maryam\Documents\m.yusuf_awariCSC101\week-6\pratice_1\target\debug\deps\pratice_1.exe: src/main.rs
+
+C:\Users\maryam\Documents\m.yusuf_awariCSC101\week-6\pratice_1\target\debug\deps\pratice_1.d: src/main.rs
+
+src/main.rs:
---
+++ week-6/pratice_1/target/debug/pratice_1.d
@@ -0,0 +1,1 @@
+C:\Users\maryam\Documents\m.yusuf_awariCSC101\week-6\pratice_1\target\debug\pratice_1.exe: C:\Users\maryam\Documents\m.yusuf_awariCSC101\week-6\pratice_1\src\main.rs
