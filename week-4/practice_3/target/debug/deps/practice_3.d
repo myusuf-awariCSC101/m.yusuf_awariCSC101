b1fc2ab7c1d47a5cbc2e18d171f9d1c7633c7a5b
maryam yusuf-awari <maryamyusufawari@gmail.com>
2024-12-04 12:46:31 -0800

    (practice 3)

---
+++ week-4/practice_3/target/debug/deps/practice_3.d
@@ -0,0 +1,5 @@
+C:\Users\maryam\Documents\m.yusuf_awariCSC101\week-4\practice_3\target\debug\deps\practice_3.exe: src/main.rs
+
+C:\Users\maryam\Documents\m.yusuf_awariCSC101\week-4\practice_3\target\debug\deps\practice_3.d: src/main.rs
+
+src/main.rs:
---
+++ week-4/practice_3/target/debug/practice_3.d
@@ -0,0 +1,1 @@
+C:\Users\maryam\Documents\m.yusuf_awariCSC101\week-4\practice_3\target\debug\practice_3.exe: C:\Users\maryam\Documents\m.yusuf_awariCSC101\week-4\practice_3\src\main.rs
