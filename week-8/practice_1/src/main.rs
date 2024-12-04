c90b38f762361c0194f8f84684a53b2b59338983
maryam yusuf-awari <maryamyusufawari@gmail.com>
2024-12-04 15:28:55 -0800

    (practice 1)

---
+++ week-8/practice_1/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+	//using vector::new()
+	let v : Vec<i64> = Vec::new();
+
+	//printing the size of vector
+	println!("\nThe length of the Vect::new is: {}",v.len());
+
+	//using macro 
+	let v = vec!["Grace", "Effing", "Basil", "Kareem", "Susan"];
+
+    //printing the size of vector
+    println!("\nThe length of vec macro is: {}",v.len())
+
+	
+}
