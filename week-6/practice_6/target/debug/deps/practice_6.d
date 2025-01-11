00bc4ccb276bbf73b3d0dd5f3142e245c00c0a71
maryam yusuf-awari <maryamyusufawari@gmail.com>
2025-01-11 15:22:41 -0800

    (practice 6)

---
+++ week-6/practice_6/target/debug/deps/practice_6.d
@@ -0,0 +1,5 @@
+C:\Users\maryam\Documents\m.yusuf_awariCSC101\week-6\practice_6\target\debug\deps\practice_6.exe: src/main.rs
+
+C:\Users\maryam\Documents\m.yusuf_awariCSC101\week-6\practice_6\target\debug\deps\practice_6.d: src/main.rs
+
+src/main.rs:
